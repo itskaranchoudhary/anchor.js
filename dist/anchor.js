8c3c95c2981962bd65106c916c4501ea6d1e009b
Karan Choudhary <karanchoudhary0826@gmail.com>
2021-10-29 15:58:10 +0530

    Update anchor.js

--- dist/anchor.js
+++ dist/anchor.js
@@ -26,6 +26,6 @@ document.addEventListener("click", function(e) {
     if (e.target.tagName == "A" &&
             !e.target.hasAttribute("target"))
     {
-        e.target.setAttribute("target", "_blank");
+        e.target.setAttribute("target", "blank");
     }
 });
